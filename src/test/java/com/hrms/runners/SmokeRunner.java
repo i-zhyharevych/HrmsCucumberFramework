182e22c040edb66bb34d361d3d98c8d312fad5f1
i-zhyharevych <ivankazhyharevych0107@gmail.com>
2020-03-08 00:03:03 -0600

    pom.xml updates

--- src/test/java/com/hrms/runners/SmokeRunner.java
+++ src/test/java/com/hrms/runners/SmokeRunner.java
@@ -7,12 +7,12 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features="src/test/resources/features"
-		, glue="com/hrms/steps"
-		, dryRun=false
-		, plugin = {"pretty","html:target/html/cucumber-default-report", "json:target/cucumber.json", "rerun:target/failed.txt"}
-		, monochrome=true
-		, tags= {"@smoke"}		
+		features = "src/test/resources/features/Login.feature",
+		glue = "com/hrms/steps",
+		dryRun = false,
+		plugin = {"pretty", "html:target/html/cucumber-default-report", "json:target/cucumber.json", "rerun:target/failed.txt" },
+		monochrome = true,
+		tags = { "@smoke" }
 		)
 public class SmokeRunner {
 
--- target/test-classes/features/GoRest.feature
+++
@@ -1,8 +0,0 @@
-Feature: Albums
-
-  @GoRest
-  Scenario: GetAlbums
-    When I provide the request data
-    And I make a call to albums API
-    Then I validate that 200 is the status code
-    And I validate the response body
